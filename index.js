d2ccdf43e68fe3bf9e7ce85c02e8eae90e62815c
jion-lee <72534237+jion-lee@users.noreply.github.com>
2021-05-26 13:22:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I love you bst 11');
+    res.send('I love you ddddd 222211');
 });
 
 app.get('/start', function(req, res){
